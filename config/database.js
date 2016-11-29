9822c9e9a453faeee0e321f53644b135fbf69343
t4ngth00 <t4ngth00@students.oamk.fi>
2016-11-29 11:04:11 +0200

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://handonme2:hoanganh2109@waffle.modulusmongo.net:27017/wobisi7J' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
